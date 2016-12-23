17d63b2b5c9c5a2008a960c0b4bcd3154bd62541
Serhat Can <serhat@opsgenie.com>
2016-12-23 08:46:30 +0300

    minor refactor

--- main.js
+++ main.js
@@ -8,18 +8,20 @@ const qs = require('querystring');
 
 exports.handler = function (event, context, callback) {
 
-    const opsgenieApiKey = process.env.opsgenieApiKey;
     const slackToken = process.env.slackToken;
-
-    opsgenie.configure({
-        'api_key': opsgenieApiKey
-    });
-
     const params = qs.parse(event.body);
+
+    // token check to make sure that requests come from Slack
     if (params.token !== slackToken) {
         console.error(`Request token (${params.token}) does not match expected`);
         return callback('Invalid request token');
     }
+
+    const opsgenieApiKey = process.env.opsgenieApiKey;
+
+    opsgenie.configure({
+        'api_key': opsgenieApiKey
+    });
 
     var create_alert_json = {
         message: params.text
